d562d9f2643b2218eb045ee30923cb4901d7952f
LorDEvilDead <lordevildead@gmail.com>
2023-08-24 23:09:53 +0300

    films rating task 1 compited

--- Gemfile
+++ Gemfile
@@ -14,3 +14,4 @@ gem 'rubocop', require: false
 gem 'rubocop-minitest', require: false
 gem 'rubocop-rake', require: false
 gem 'test-unit'
+gem 'byebug'
--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,10 +4,13 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        rating = film{"rating_kinopoisk"}
-        film_two_countries = film["country"].to_s.include?(",")
-        film_two_countries.reduce(0) {|sum, rating| sum + rating} / film_two_countries.lenght
+      def rating(array)
+        films_average = array.map do |film_two_countries| 
+          if film_two_countries["country"].to_s.include?(",") && film_two_countries["rating_kinopoisk"].to_f > 0
+            film_two_countries["rating_kinopoisk"]
+          end 
+        end
+        films_average.compact!.reduce(0) {|sum, each_average| sum + each_average.to_f} / films_average.length 
       end
 
       def chars_count(_films, _threshold)
