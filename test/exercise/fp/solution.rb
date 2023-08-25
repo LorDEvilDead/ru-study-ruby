8e0a7d70bb20e647dc4289b8e19d67414b720c07
LorDEvilDead <lordevildead@gmail.com>
2023-08-25 15:14:18 +0300

    share to macOS

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,8 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(array)
-        films_average = array.map do |film_two_countries| 
+      def rating(_array)
+        films_average = _array.map do |film_two_countries| 
           if film_two_countries["country"].to_s.include?(",") && film_two_countries["rating_kinopoisk"].to_f > 0
             film_two_countries["rating_kinopoisk"]
           end 
@@ -14,7 +14,12 @@ module Exercise
       end
 
       def chars_count(_films, _threshold)
-        0
+        films_average = _array.map do |film_two_countries|
+        if film_two_countries["rating_kinopoisk"].to_f >= _threshold
+          film_two_countries["name"]
+        end
+        films_average.compact!
+        
       end
     end
   end
