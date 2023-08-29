1348b4e282d38064d6f650b9917fca4f86e3d9d9
Pavel <lordevildead@gmail.com>
2023-08-29 11:39:51 +0300

    share to pc

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -6,38 +6,40 @@ module Exercise
 
       # Написать свою функцию my_each
     
-      def my_each (&block)
-        if MyArray.any? && block.persisted
-          block.call
-        else   
-          return @MyArray
-        end
+      def my_each 
+        if block_given?
+          arr = []
+          for element in arr do
+            block.call
+            else
+              <Enumerator:... :my_each>
+            end
+          end
+        end  
       end
       # 1. нужно условие если есть блок сделай одно, если нет - другое
       # 2. массив может быть? (заполнинный и?)
-      # 3. block.call(x)      
-      end
-
-      [1,2,3,4]
-       0 1 2 3
-      a.my_each do |x| 
-        puts x * 2
-      end
+      # 3. block.call(x) 
+      # [1,2,3,4]
+      #  0 1 2 3
+      # a.my_each do |x| 
+      #   puts x * 2
+      # end
 
 
       # Написать свою функцию my_map
       def my_map
-      
+      0
       end
 
       # Написать свою функцию my_compact
       def my_compact 
-      
+      0
       end
 
       # Написать свою функцию my_reduce
       def my_reduce 
-      
+      0
       end
     end
   end
--- test/exercise/fp2/test.rb
+++ test/exercise/fp2/test.rb
@@ -22,12 +22,14 @@ class Exercise::Fp2Test < Minitest::Test
   end
 
   def test_my_map
+    skip
     func = ->(element) { element * @int }
     assert @array.map(&func) == @my_array.my_map(&func)
     assert @array.map(&func).map(&func) == @my_array.my_map(&func).my_map(&func)
   end
 
   def test_my_compact
+    skip
     func = ->(element) { element if element.even? }
     func_another = ->(element) { element * @int }
     func_yet_another = ->(element) { element.even? }
@@ -37,8 +39,8 @@ class Exercise::Fp2Test < Minitest::Test
   end
 
   def test_my_reduce
+    skip
     func = ->(acc, element) { acc * element }
-skip
     assert @array.reduce(&func) == @my_array.my_reduce(&func)
     assert @array.reduce(2, &func) == @my_array.my_reduce(2, &func)
     assert @array.reduce(&:+) == @my_array.my_reduce(&:+)
