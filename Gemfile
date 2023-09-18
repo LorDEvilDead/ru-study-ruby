66c01500a11a12a48c453a4df2a3b0be88c506aa
Pavel <lordevildead@gmail.com>
2023-09-18 16:21:50 +0300

    Final fixes linter -check, tests -check

--- Gemfile
+++ Gemfile
@@ -13,4 +13,3 @@ gem 'rubocop', require: false
 gem 'rubocop-minitest', require: false
 gem 'rubocop-rake', require: false
 gem 'test-unit'
-gem 'pry'
--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,58 +1,30 @@
-require 'pry'
 module Exercise
   module Arrays
     class << self
-
       def my_max(arr)
         max = arr[0]
         arr.each do |num|
           max = num if num > max
         end
         max
-      end 
+      end
 
       def replace(array)
         array.map { |number| number.positive? ? my_max(array) : number }
       end
- 
-      def search(array, query, low=0, high=array.length-1)
-        if low > high
-          return -1
-        else
-          mid = (low+high)/2
-        if  query < array[mid]
-          search(array, query, mid-1, high=mid)
+
+      def search(array, query, low = 0, high = array.length - 1)
+        return -1 if low > high
+
+        mid = (low + high) / 2
+        if query < array[mid]
+          search(array, query, low, mid - 1)
         elsif query > array[mid]
-          search(array, query, mid+1, low=array[mid])
+          search(array, query, mid + 1, high)
         else
-          return mid
+          mid
         end
       end
     end
   end
 end
-[1,2,3,4,5,6,7]
-# def search(array, query)
-# low = 0
-# high = array.length-1
-# result = -1
-# while low <= high
-#   mid = (low+high)/2
-#   if query == array[mid]
-#     result = mid
-#     break
-#   end
-#   if query > array[mid]
-#    low = mid + 1
-#   end
-#   if query < array[mid]
-#     high = mid - 1
-#   end
-# end
-# return result
-              
-
-   
-
-
-
