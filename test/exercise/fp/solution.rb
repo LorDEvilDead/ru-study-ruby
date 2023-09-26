3055479c8ec9250d16f98f4375eed633755c5ceb
LorDEvilDead <lordevildead@gmail.com>
2023-09-26 14:31:04 +0300

    rename: films_reting_filter to film & films_choosen to choosen_films

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -6,17 +6,17 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        films_reting_filter = array.map do |each_film|
+        film = array.map do |each_film|
           each_film if each_film['country'].to_s.split(',').length >= 2 && each_film['rating_kinopoisk'].to_f.positive?
         end
-        films_reting_filter.compact!.reduce(0) { |sum, rating| sum + rating['rating_kinopoisk'].to_f } / films_reting_filter.length
+        film.compact!.reduce(0) { |sum, rating| sum + rating['rating_kinopoisk'].to_f } / film.length
       end
 
       def chars_count(films, threshold)
-        films_choosen = films.map do |film|
+        choosen_films = films.map do |film|
           film if film['rating_kinopoisk'].to_f >= threshold
         end
-        films_choosen.compact.reduce(0) do |sum, each_film|
+        choosen_films.compact.reduce(0) do |sum, each_film|
           sum + each_film['name'].count('и')
         end
       end
